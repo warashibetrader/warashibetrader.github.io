90885c25af64125b49278d6e6235a57dbaca07e2
warashibetrader <96086771+warashibetrader@users.noreply.github.com>
2022-02-24 15:39:45 -0800

    Update service-worker.js

--- crypto/service-worker.js
+++ crypto/service-worker.js
@@ -49,8 +49,7 @@ self.addEventListener('fetch', (evt) => {
 	if (evt.request.url.startsWith(self.location.origin)) evt.respondWith(
 		caches.open(CACHE_NAME).then((cache) => {
 		return cache.match(evt.request).then((response) => {
-			if (response) console.log('[sw] Cache fetch', evt.request.url);
-			else console.log('[sw] Network fetch', evt.request.url);
+			if (!response) console.log('[sw] Network fetch', evt.request.url);
 			return response || fetch(evt.request).catch(() => {
 				return cache.match('wallet.html');		
 			});
